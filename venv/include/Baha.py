f253d9018a532a61b7021baef84ad1715d1e72d1
CoderBaha11 <probaha2011@gmail.com>
2021-12-04 20:12:23 +0100

    first

---
+++ venv/include/Baha.py
@@ -0,0 +1,29 @@
+a = 10
+b = 15
+print (a + b)
+
+#Bu program konsoldan kullanici tarafindan girilen 2rakami toplarve konsola yazdirir
+#Bu programda konsolda data girmeyi oreniyoruz
+#tolpama islemi yaptirmayi ve
+#konsola yazmayi oreniyoruz
+#Ekrandan bilgi girme input
+#Asagidaki satir konsoldan girilen degeri a diskene aktarir
+print ("lutfen a sayisini giriniz:")
+a = int (input ())
+b = int (input ("lutfen b sayisini giriniz: "))
+#Asagidaki satir toplama islemini yapar
+#bilgisayar deger aktarma islemini sagdan sola doru yapar
+sum = a + b
+#cikarma islemlerini yapar
+fark = a - b
+#carpma islemlerini yapar
+carpim = a * b
+#bolme islemlerini yapar
+bolme = a / b
+kalan = a % b
+#Asagidaki satir toplam degerini konsola yazar
+print ("a ve b toplami:", sum)
+print ("a ve b farki:", fark)
+print ("a ve b carpimi:", carpim)
+print ("a ve b bolme :", bolme)
+print ("a ve b bolumunden kalan: ", kalan)
---
+++ venv/include/matematik.py
@@ -0,0 +1,27 @@
+yunus="pear"
+said="apple"
+ahmed="mandarin"
+esat="lemon"
+harun="grape"
+print("my friends fruit list: " ,yunus,said ,ahmed,esat,harun)
+
+print("yunus :" ,yunus ,", said : ",said ,", ahmed:",ahmed ,", esat:",esat ,", harun:" ,harun)
+
+first_gool=121
+second_gool=132
+third_gool=108
+print("result of three points")
+print("result of three points: ",first_gool+second_gool+third_gool)
+
+print("first_gool:",first_gool)
+print("second_gool:",second_gool)
+print("third_gool:",third_gool)
+
+
+print("first_gool:",first_gool,",second_gool:",second_gool,",third_gool:",third_gool)
+
+print("difference between first and second points:",second_gool-first_gool)
+
+
+# input("select one of these keys: r,p,s")
+

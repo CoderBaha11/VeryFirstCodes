9a4a313c8a0c4a090e353797f99975b46cd63b77
CoderBaha11 <probaha2011@gmail.com>
2021-12-04 20:27:42 +0100

    something added

--- venv/include/matematik.py
+++ venv/include/matematik.py
@@ -20,7 +20,8 @@ print("third_gool:",third_gool)
 
 print("first_gool:",first_gool,",second_gool:",second_gool,",third_gool:",third_gool)
 
-print("difference between first and second points:",second_gool-first_gool)
+print("difference between first and second points:",second_gool - first_gool)
+print("difference between second and third points:",third_gool - second_gool)
 
 
 # input("select one of these keys: r,p,s")
